529cdfd804aa7ad6d5721bc5eb579219f0c40833
Viduni-Hewage <viduhewage02@gmail.com>
2025-04-21 19:18:50 +0530

    Modify App navigation page

--- src/navigation/AppNavigation.tsx
+++ src/navigation/AppNavigation.tsx
@@ -3,6 +3,7 @@ import MainScreen from "../screens/Main.screen";
 import LoginScreen from "../screens/Login.screen";
 import LoadingScreen from "../screens/Loading.screen";
 import SignInScreen from "../screens/SignIn.screen";
+import FirstadScreen from "../screens/Firstad.screen";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 const Stack = createNativeStackNavigator();
@@ -14,6 +15,7 @@ const AppNavigation = () =>{
             <Stack.Screen name = "Main" component={MainScreen} />
             <Stack.Screen name = "Login" component={LoginScreen} options = {{headerShown: false}}/>
             <Stack.Screen name = "SignIn" component={SignInScreen} options = {{headerShown: false}}/>
+            <Stack.Screen name = "Firstad" component={FirstadScreen} options = {{headerShown: false}}/>
         </Stack.Navigator>
     );
 };
