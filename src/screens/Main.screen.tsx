88d6b25aef496e58f58f77f55bdbab8599013687
Viduni-Hewage <viduhewage02@gmail.com>
2025-04-22 21:05:07 +0530

    Modify main page

--- src/screens/Main.screen.tsx
+++ src/screens/Main.screen.tsx
@@ -1,15 +1,47 @@
-import {Button, View, Text} from 'react-native';
+import React from 'react';
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import styles from '../styles/Main.styles';
+import LinearGradient from 'react-native-linear-gradient';
 
-const MainScreen = ({navigation}: any) =>{
-    return (
-        <View>
-            <Text>Main screen</Text>
-            <Button
-                title = "Get Started"
-                onPress = {() => navigation.navigate('Login')}
+const StartScreen = () => {
+  const navigation = useNavigation();
+
+  return (
+    <LinearGradient colors={['#2D8ED7', '#693FDC']} style={styles.gradient}>
+        <View style={styles.container}>
+            <Image
+                source={require('../assets/logoBig.png')}
+                style={styles.image}
+                resizeMode="contain"
             />
+
+            <Text style={styles.title}>DOO-IT</Text>
+
+            <View style={{ flex: 1 }} />
+
+            <TouchableOpacity
+                style={styles.button}
+                onPress={() => navigation.navigate('Login' as never)}
+            >
+                <View style={styles.buttonContent}>
+                    <Text style={styles.buttonText}>Get Started</Text>
+                    <Image
+                        source={require('../assets/front.png')}
+                        style={styles.arrow}
+                        resizeMode="contain"
+                    />
+                </View>
+
+            </TouchableOpacity>
         </View>
-    );
+    </LinearGradient>
+  );
 };
 
-export default MainScreen;
+export default StartScreen;
---
+++ src/styles/Main.styles.ts
@@ -0,0 +1,51 @@
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { StyleSheet } from 'react-native';
+
+const styles = StyleSheet.create({
+    gradient: {
+        flex: 1,
+      },
+    container: {
+      flex: 1,
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      paddingTop: hp('12%'),
+      paddingHorizontal: wp('5%'),
+    },
+    image: {
+      width: 120,
+      height: 120,
+      marginTop: hp('13%'),
+    },
+    title: {
+      marginTop: hp('5%'),
+      fontSize: 25,
+      fontWeight: 900,
+      color: 'white',
+
+    },
+    button: {
+      position: 'absolute',
+      bottom: hp('8%'),
+      width: '80%',
+      paddingVertical: 15,
+      borderRadius: 25,
+      alignItems: 'center',
+    },
+    buttonText: {
+      color: '#fff',
+      fontSize: 18,
+      fontWeight: 'bold',
+    },
+    buttonContent: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+    arrow: {
+        width: 50,
+        height: 20,
+    },
+  });
+
+export default styles;
